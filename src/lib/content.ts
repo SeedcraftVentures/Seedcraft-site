930d8c70e9c9213def0f040804295429bf8bbe93
NicheQuiche94 <ace.lemaitre@gmail.com>
2026-06-15 09:27:48 +0100

    partner images

--- src/lib/content.ts
+++ src/lib/content.ts
@@ -158,9 +158,8 @@ export const team = {
   sub: 'A small team that builds the real thing, proves it, and stays in for the long run.',
   members: [
     { src: '/Images/AndreSC.jpg', name: 'Andre Lemaitre', role: 'Founder, Seedcraft Ventures' },
-    { photo: '1494790108377-be9c29b29330', name: 'Ashley Goluoglu', role: 'Founder, Harika Labs, Partner' },
-    // TODO: third member to be confirmed
-    { photo: '1500648767791-00dcc994a43e', name: 'Builder', role: 'Product' },
+    { src: '/Images/ashley.png', name: 'Ashley Goluoglu', role: 'Founder, Harika Labs, Partner' },
+    { src: '/Images/Gabri.jpg', name: 'Gabriele Aimone', role: 'Founder, Grit Studios, Venture Partner (Smokeless)' },
   ] as TeamMember[],
 }
 
